ec25c0a73a4772fd1d17d568db653906774df36c
emilyvera <evera5@illinois.edu>
2018-10-03 20:46:15 -0500

    submit

--- src/App.js
+++ src/App.js
@@ -12,13 +12,45 @@ class App extends Component {
     ]
   };
 
+  handleDelete = event => {
+    var newTodoList = [...this.state.todos];
+    var index = newTodoList.indexOf(event.target.value);
+    newTodoList.splice(index, 1);
+    this.setState({todos: newTodoList});
+  };
+
+  handleChange = event => {
+      this.setState({ newTodo: event.target.value });
+    };
+
+  handleSubmit = event => {
+      event.preventDefault();
+      this.setState({
+        todos: [...this.state.todos, this.state.newTodo],
+        newTodo: ""
+      });
+    };
+
   render() {
     return (
       <div>
         <h1> How to make boba at home</h1>
-        {this.state.todos.map(todo => (
-          <li key={todo}> {todo}</li>
-        ))}
+        {this.state.todos.map(todo =>
+          <div>
+            <li key={todo}>
+              {todo + " "}
+              <button  value={todo} onClick={this.handleDelete}>X</button>
+
+                <input
+                  type="text"
+                  value={this.state.newTodo}
+                  onChange={this.handleChange}
+                />
+                <button onClick={this.handleSubmit}> + </button>
+
+            </li>
+          </div>
+        )}
       </div>
     );
   }
